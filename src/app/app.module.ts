97dc3bdd34e06db5ed45ed1d12e60f1e5ee19e41
Mohammad Hamza <hmohdhamza@gmail.com>
2024-02-02 01:36:43 +0530

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiWidgetsModule } from '../../libs/ui-widgets/src/lib/ui-widgets.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UiWidgetsModule
+    UiWidgetsModule 
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
