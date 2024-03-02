514a38a3b31e4e9404243b335efd0c429d33e4f6
Mohammad Hamza <hmohdhamza@gmail.com>
2024-03-02 16:33:23 +0530

    initial backend setup

---
+++ backend/src/test/java/com/gameswitch/GameswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameswitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
